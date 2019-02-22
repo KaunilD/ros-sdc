72b582e558cad2d5c32f05fecba07c9100910ba7
Kaunil Dhruv <dhruv.kaunil@gmail.com>
2019-02-21 18:49:42 -0700

    moved files

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/dhruv/ros/self_driving_car/src
+# Build directory: /home/dhruv/ros/self_driving_car/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
